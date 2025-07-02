31c7f32ce54b81de60e5868457f5d57584d54521
Vikraman Arumugam <vickyvlad1412@gmail.com>
2025-07-02 13:00:02 +0800

    Revert "JDK changes for windows version"
    This reverts commit 80e251a4b365c93411752bda6ceee3b9a499a143.

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -1,5 +1,3 @@
-#include <jni.h>
-
 #include <flutter/dart_project.h>
 #include <flutter/flutter_view_controller.h>
 #include <windows.h>
@@ -33,26 +31,6 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
-
-  JavaVM* jvm = nullptr;
-  JNIEnv* env = nullptr;
-  JavaVMOption options[1];
-  std::string classPath = "-Djava.class.path="
-       R"(C:\path\to\big-math-2.3.2.jar;C:\path\to\ExpressionEvaluator.jar)";
-  options[0].optionString = const_cast<char*>(classPath.c_str());
-
-  JavaVMInitArgs vm_args = {};
-  vm_args.version = JNI_VERSION_1_8;
-  vm_args.nOptions = 1;
-  vm_args.options = options;
-  vm_args.ignoreUnrecognized = JNI_FALSE;
-
-  if (JNI_CreateJavaVM(&jvm, reinterpret_cast<void**>(&env), &vm_args) < 0) {
-    MessageBox(nullptr, L"Failed to start JVM", L"Error", MB_OK);
-    // decide: abort or continue without JNI
-  }
-
-
 
   ::MSG msg;
   while (::GetMessage(&msg, nullptr, 0, 0)) {
